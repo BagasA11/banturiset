f615e5c5b01d4cb6e357513aede318a5e604cc04
BagasA112 <bagasrayhan03@gmail.com>
2024-07-24 17:27:14 +0700

    change cors allowed source

--- backend/middleware/cors.middleware.go
+++ backend/middleware/cors.middleware.go
@@ -6,7 +6,7 @@ import (
 
 func EnableCors() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, DELETE, GET, PUT")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
