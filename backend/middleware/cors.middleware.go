0cb5ede5b2115420e246a9190573d44c5f735f14
BagasA112 <bagasrayhan03@gmail.com>
2024-07-24 18:44:43 +0700

    set OPTIONS allowd method

--- backend/middleware/cors.middleware.go
+++ backend/middleware/cors.middleware.go
@@ -8,8 +8,12 @@ func EnableCors() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, DELETE, GET, PUT")
+		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, POST, DELETE, GET, PUT")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.AbortWithStatus(200)
+			return
+		}
 		ctx.Next()
 	}
 }
